3bd5ba6df71f20a5d56a0bd15598d30302179a78
carupisu0320 <nekohaoki215@gmail.com>
2026-04-19 06:45:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
-alert("main.js読み込まれた！");
+let hand = [
+  { top: 1, bottom: 2 },
+  { top: 3, bottom: 4 },
+  { top: 5, bottom: 6 },
+  { top: 2, bottom: 2 },
+  { top: 6, bottom: 1 }
+]
 
-function test() {
-  alert("成功！");
+let field = []
+
+function render() {
+  const handDiv = document.getElementById("hand")
+  const fieldDiv = document.getElementById("field")
+
+  handDiv.innerHTML = "<h2>手札</h2>"
+  fieldDiv.innerHTML = "<h2>場</h2>"
+
+  hand.forEach((card, index) => {
+    const div = document.createElement("div")
+    div.innerText = `${card.top}-${card.bottom}`
+    div.onclick = () => discard(index)
+    handDiv.appendChild(div)
+  })
+
+  field.forEach(card => {
+    const div = document.createElement("div")
+    div.innerText = `${card.top}-${card.bottom}`
+    fieldDiv.appendChild(div)
+  })
 }
+
+function draw() {
+  const newCard = {
+    top: Math.ceil(Math.random() * 6),
+    bottom: Math.ceil(Math.random() * 6)
+  }
+
+  hand.push(newCard)
+  render()
+}
+
+function discard(index) {
+  const card = hand.splice(index, 1)[0]
+  field.push(card)
+  render()
+}
+
+render()
