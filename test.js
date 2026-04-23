8d69722bed5ddc3b6aacedd09cae093ecf24afcb
carupisu0320 <nekohaoki215@gmail.com>
2026-04-23 12:33:09 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
-// ===== テスト用手札 =====
-
-// 一色
 function setIsshiki() {
   hand = [
     {top:1,bottom:2,faceUp:true},
@@ -13,13 +10,11 @@ function setIsshiki() {
   render()
 }
 
-// 三連
 function setSanren() {
   hand = [
     {top:1,bottom:3,faceUp:true},
     {top:2,bottom:3,faceUp:true},
     {top:3,bottom:3,faceUp:true},
-
     {top:4,bottom:5,faceUp:true},
     {top:5,bottom:5,faceUp:true},
     {top:6,bottom:5,faceUp:true}
@@ -27,7 +22,6 @@ function setSanren() {
   render()
 }
 
-// 六華
 function setRikka() {
   hand = [
     {top:1,bottom:4,faceUp:true},
