a35ee60ee9d6cbe18ff2f55dbafe9326b4dffaf1
carupisu0320 <nekohaoki215@gmail.com>
2026-04-23 13:50:53 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,35 +1,32 @@
-function setIsshiki() {
-  hand = [
-    {top:1,bottom:2,faceUp:true},
-    {top:3,bottom:2,faceUp:true},
-    {top:4,bottom:2,faceUp:true},
-    {top:5,bottom:2,faceUp:true},
-    {top:6,bottom:2,faceUp:true},
-    {top:2,bottom:2,faceUp:true}
-  ]
-  render()
-}
+function testRole(type) {
+  let h = []
 
-function setSanren() {
-  hand = [
-    {top:1,bottom:3,faceUp:true},
-    {top:2,bottom:3,faceUp:true},
-    {top:3,bottom:3,faceUp:true},
-    {top:4,bottom:5,faceUp:true},
-    {top:5,bottom:5,faceUp:true},
-    {top:6,bottom:5,faceUp:true}
-  ]
-  render()
-}
+  if(type==="rikka"){
+    for(let i=1;i<=6;i++) h.push({top:i,bottom:3,faceUp:true})
+  }
 
-function setRikka() {
-  hand = [
-    {top:1,bottom:4,faceUp:true},
-    {top:2,bottom:4,faceUp:true},
-    {top:3,bottom:4,faceUp:true},
-    {top:4,bottom:4,faceUp:true},
-    {top:5,bottom:4,faceUp:true},
-    {top:6,bottom:4,faceUp:true}
-  ]
+  if(type==="sanren"){
+    h=[
+      {top:1,bottom:2,faceUp:true},
+      {top:2,bottom:2,faceUp:true},
+      {top:3,bottom:2,faceUp:true},
+      {top:4,bottom:5,faceUp:true},
+      {top:5,bottom:5,faceUp:true},
+      {top:6,bottom:5,faceUp:true},
+    ]
+  }
+
+  if(type==="isshiki"){
+    h=[
+      {top:1,bottom:4,faceUp:true},
+      {top:3,bottom:4,faceUp:true},
+      {top:5,bottom:4,faceUp:true},
+      {top:2,bottom:4,faceUp:true},
+      {top:6,bottom:4,faceUp:true},
+      {top:4,bottom:4,faceUp:true},
+    ]
+  }
+
+  hand = h
   render()
 }
